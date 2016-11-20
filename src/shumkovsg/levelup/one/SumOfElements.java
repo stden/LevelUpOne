a8260ff36946ceb4373876eb35d2318314e50af7
m06ssg <m06ssg@gmail.com>
2016-11-20 23:51:44 +0300

    Добавлен класс SumOfElements.java.
    В классе MinSearchAlg.java усовершенствован ввод размера массива.
    
    Обновлено README.

--- src/shumkovsg/levelup/one/SumOfElements.java
+++ src/shumkovsg/levelup/one/SumOfElements.java
@@ -1,15 +1,13 @@
 package shumkovsg.levelup.one;
 
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by sergey on 20.11.16.
  */
-public class MinSearchAlg {
+public class SumOfElements {
     public static void main(String[] args) {
-
         System.out.println("Searching minimum number im array!");
         System.out.print("Enter size of array: ");
 
@@ -27,23 +25,22 @@ public class MinSearchAlg {
         System.out.println("Your array is:");
         System.out.println(Arrays.toString(array));
 
-        System.out.print("Min element is: ");
-        System.out.println(minSearch(array));
-
-
+        System.out.println("Sum of elements in array is: " + sumOfElements(array));
     }
 
-    private static double minSearch(double[] arr) {
-        if (arr.length > 1) {
-            double min = arr[0];
-            for (int i = 1; i < arr.length; i++) {
-                if (min > arr[i]) {
-                    min = arr[i];
+    private static int readArraySize(Scanner in) {
+        int arraySize;
+        while (true) {
+            if (in.hasNextInt()) {
+                if ((arraySize = in.nextInt()) <= 0) {
+                    System.out.print("Size of array must be above zero! Try again: ");
+                } else {
+                    return arraySize;
                 }
+            } else {
+                System.out.print("Size of array must be integer! Try again: ");
+                in.next();
             }
-            return min;
-        } else  {
-            return arr[0];
         }
     }
 
@@ -63,19 +60,11 @@ public class MinSearchAlg {
         }
     }
 
-    private static int readArraySize(Scanner in) {
-        int arraySize;
-        while (true) {
-            if (in.hasNextInt()) {
-                if ((arraySize = in.nextInt()) <= 0) {
-                    System.out.print("Size of array must be above zero! Try again: ");
-                } else {
-                    return arraySize;
-                }
-            } else {
-                System.out.print("Size of array must be integer! Try again: ");
-                in.next();
-            }
+    private static double sumOfElements(double[] arr) {
+        double sum = 0;
+        for (double anArr : arr) {
+            sum += anArr;
         }
+        return sum;
     }
 }
